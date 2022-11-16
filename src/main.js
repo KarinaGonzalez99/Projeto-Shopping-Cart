d9657e5cb3af67a9f7efb03857bea80d70c8d674
Karina Gonzalez <karinagonzalez99@gmail.com>
2022-11-16 16:09:29 -0300

    Iniciando mais um projetinho!!! Front-end

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 import { searchCep } from './helpers/cepFunctions';
-import './style.css';
+import './style.css'; // iniciando projeto
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
