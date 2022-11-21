0e873d261358c39270b664fdd3a722ca678c430c
Karina Gonzalez <karinagonzalez99@gmail.com>
2022-11-21 16:51:25 -0300

    requisito 6

--- src/tests/fetchProduct.test.js
+++ src/tests/fetchProduct.test.js
@@ -4,7 +4,14 @@ import product from './mocks/product';
 
 // implemente seus testes aqui
 describe('Teste a função fetchProduct', () => {
-  it('...', () => {
-    
+  it('fetchProducts é uma função', () => {
+    expect(typeof fetchProduct).toBe('function');
   });
+  it('fetch é chamado ao executar fetchProduct', async () => {
+    await fetchProduct("MLB1405519561");
+    expect(fetch).toContainEqual();
+  });
+  it('fetchProduct sem argumento, retorna um erro com a mensagem', async () => {
+    await expect(fetchProduct()).rejects.toThrow(new Error("ID não informado"));
+});
 });
